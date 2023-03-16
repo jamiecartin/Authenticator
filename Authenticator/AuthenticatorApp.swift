1b0d09f05b91cf0798addf86f788a21b81f78bf5
jamiecartin <jamie.cartin@icloud.com>
2023-03-16 14:27:08 -0400

    Initial Commit

---
+++ Authenticator/AuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorApp.swift
+//  Authenticator
+//
+//  Created by Jamie Cartin on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
